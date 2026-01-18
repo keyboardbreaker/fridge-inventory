9b6c204e9103838205f962ccf9c05f040fa6b553
keyboardbreaker <yujin_1991@hotmail.com>
2026-01-18 12:52:44 +0000

    feat: display fridge tables in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import supabase from '../utils/supabase'
+import type { Fridge } from './types/fridge';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fridges, setFridges] = useState<Fridge[]>([]);
+
+  useEffect(() => {
+    async function getFridges() {
+
+      const { data, error } = await supabase
+        .from("fridges")
+        .select("*");
+
+      if (error) {
+        console.error(error);
+        return;
+      }
+      if (data) { //shorthand setFridges(data ?? []);
+        setFridges(data)
+      }
+    }
+
+    getFridges()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Fridges</h1>
+      <ul>
+        {fridges.map((fridge) => (
+          <li key={fridge.id}>{fridge.name}</li>
+        ))}
+      </ul>
     </>
   )
 }
-
 export default App

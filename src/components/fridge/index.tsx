4789d5e48ba2c800c7fd1c6f74a407c611465088
keyboardbreaker <yujin_1991@hotmail.com>
2026-01-25 03:30:17 +0000

    feat: loading the fridge within the fridge component

--- src/components/fridge/index.tsx
+++ src/components/fridge/index.tsx
@@ -1,9 +1,40 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import supabase from "../../../utils/supabase";
+import type { Fridge as FridgeType } from "../../types/fridge";
+
 const Fridge = () => {
-	return (
-		<section>
-			This will show a fridge
-		</section>
-	)
+	const { fridgeId } = useParams<{fridgeId : string}>();
+	const [fridge, setFridge] = useState<FridgeType | null> (null);
+
+	useEffect(() => {
+		if(!fridgeId) return;
+
+		async function loadFridge() {
+			const { data, error } = await supabase
+				.from("fridges")
+				.select("id, name, created_at")
+				.eq("id", fridgeId)
+				.single();
+			
+			if(error) {
+				console.error(error);
+				return;
+			}
+
+			setFridge(data);
+		}
+		loadFridge();
+	}, [fridgeId]);
+
+  if (!fridge) return <p>Loading…</p>;
+
+  return (
+    <>
+      <h1>{fridge.name}</h1>
+      <p>ID: {fridge.id}</p>
+    </>
+  );
 }
 
 export default Fridge;

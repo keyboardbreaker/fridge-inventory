706b8a631ae73c217b063094eece8fe185e21869
keyboardbreaker <yujin_1991@hotmail.com>
2026-01-25 12:59:00 +0000

    feat: querying fridges to get current existing food items

--- src/components/fridge/index.tsx
+++ src/components/fridge/index.tsx
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import supabase from "../../../utils/supabase";
-import type { Fridge as FridgeType } from "../../types/fridge";
+
+type FoodItem = {
+  id: string;
+  name: string;
+  share_status: "private" | "shared" | "ask";
+  best_before_date: string | null;
+  quantity: number | null;
+  unit: string | null;
+};
+
+type FridgeWithFood = {
+  id: string;
+  name: string;
+  food_items: FoodItem[];
+};
 
 const Fridge = () => {
 	const { fridgeId } = useParams<{fridgeId : string}>();
-	const [fridge, setFridge] = useState<FridgeType | null> (null);
+	const [fridge, setFridge] = useState<FridgeWithFood | null> (null);
 
 	useEffect(() => {
 		if(!fridgeId) return;
@@ -13,7 +27,19 @@ const Fridge = () => {
 		async function loadFridge() {
 			const { data, error } = await supabase
 				.from("fridges")
-				.select("id, name, created_at")
+				.select(`
+					id,
+					name,
+					food_items (
+						id,
+						name,
+						share_status,
+						best_before_date,
+						quantity,
+						unit
+					),
+					created_at
+				`)
 				.eq("id", fridgeId)
 				.single();
 			
@@ -32,7 +58,17 @@ const Fridge = () => {
   return (
     <>
       <h1>{fridge.name}</h1>
-      <p>ID: {fridge.id}</p>
+      <ul>
+				{fridge.food_items.length === 0 && (
+          <li>No food yet</li>
+        )}
+
+				{fridge.food_items.map((item) => (
+          <li key={item.id}>
+            {item.name} — {item.share_status}
+          </li>
+        ))}
+			</ul>
     </>
   );
 }

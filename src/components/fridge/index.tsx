8a28d42d66e15617781f2ea269d9d4e70c1d1f09
keyboardbreaker <yujin_1991@hotmail.com>
2026-06-11 16:10:39 +0100

    feat: replacing fridge list with a table

--- src/components/fridge/index.tsx
+++ src/components/fridge/index.tsx
@@ -92,44 +92,62 @@ const Fridge = () => {
 			<Container>
 				<h1>{fridge.name}</h1>
 
-			<button
-				onClick={() => setShowModal(true)}
-			>
-				Add New Food Item
-			</button>
-			{showModal && (
-				<div className={style.backdrop}>
-					<div className={style.modal}>
-						<div className={style.modalHeader}>
-						<h2>Add food Item</h2>
-						<button
-							className={style.closeButton}
-							onClick={() => setShowModal(false)}
-						>x</button>
-					</div>
+				<button
+					onClick={() => setShowModal(true)}
+				>
+					Add New Food Item
+				</button>
+				{showModal && (
+					<div className={style.backdrop}>
+						<div className={style.modal}>
+							<div className={style.modalHeader}>
+							<h2>Add food Item</h2>
+							<button
+								className={style.closeButton}
+								onClick={() => setShowModal(false)}
+							>x</button>
+						</div>
 
-					<AddFoodForm
-						fridgeId={fridge.id}
-						onFoodAdded={async () => {
-							await loadFridge();
-							setShowModal(false);
-						}}
-					/>
+						<AddFoodForm
+							fridgeId={fridge.id}
+							onFoodAdded={async () => {
+								await loadFridge();
+								setShowModal(false);
+							}}
+						/>
+						</div>
 					</div>
+				)}
+
+				<div className={style.fridgeContainer}>
+					<table style={{ width: '100%', borderCollapse: 'collapse' }}>
+						<thead>
+							<tr style={{ backgroundColor: 'indigo',  }}>
+								<th className={style.cellStyle}>ID</th>
+								<th className={style.cellStyle}>Name</th>
+								<th className={style.cellStyle}>Email</th>
+								<th className={style.cellStyle}>Best before date</th>
+								<th className={style.cellStyle}>Delete?</th>
+							</tr>
+						</thead>
+						<tbody>
+							{fridge.food_items.length === 0 && (
+								<li>The fridge is empty</li>
+							)}
+							{fridge.food_items.map((item) => (
+								<tr key={fridge.id} style={{ borderBottom: '1px solid #ddd' }}>
+									<td className={style.cellStyle}>{item.id}</td>
+									<td className={style.cellStyle}>{item.name}</td>
+									<td className={style.cellStyle}>{item.share_status}</td>
+									<td className={style.cellStyle}>{item.best_before_date}</td>
+									<td className={style.cellStyle}>
+										<button onClick={() => deleteFoodItem(item.id)}>Delete</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
 				</div>
-			)}
-				<ul className={style.fridgeContainer}>
-					{fridge.food_items.length === 0 && (
-						<li>The fridge is empty</li>
-					)}
-
-					{fridge.food_items.map((item) => (
-						<li key={item.id}>
-							<span>{item.name} — {item.share_status}</span>
-							<button onClick={() => deleteFoodItem(item.id)}>Delete</button>
-						</li>
-					))}
-				</ul>
 			</Container>
 		</>
 	);

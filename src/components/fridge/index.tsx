9ca20acc408195329198a1a7d837e8e842d47330
keyboardbreaker <yujin_1991@hotmail.com>
2026-06-11 15:04:44 +0100

    feat: user can delete fooditem from fridge

--- src/components/fridge/index.tsx
+++ src/components/fridge/index.tsx
@@ -55,9 +55,35 @@ const Fridge = () => {
 		setFridge(data);
 	}
 
+	const deleteFoodItem =  async(foodItemId: string) => {
+		const { error } = await supabase
+			.from("food_items")
+			.delete()
+			.eq("id", foodItemId);
+
+		if (error) {
+			console.error(error);
+			return;
+		}
+
+		await loadFridge();
+	}
+
 	useEffect(() => {
 		void loadFridge();
 	}, [fridgeId]);
+
+	useEffect(() => {
+		const checkUser = async () => {
+			const {
+			data: { user },
+			} = await supabase.auth.getUser();
+
+			console.log("Current user:", user?.id);
+		}
+
+		checkUser();
+	}, []);
 
 	if (!fridge) return <Loader/>;
 
@@ -66,7 +92,7 @@ const Fridge = () => {
 			<Container>
 				<h1>{fridge.name}</h1>
 
-							<button
+			<button
 				onClick={() => setShowModal(true)}
 			>
 				Add New Food Item
@@ -93,15 +119,16 @@ const Fridge = () => {
 				</div>
 			)}
 				<ul className={style.fridgeContainer}>
-						{fridge.food_items.length === 0 && (
-							<li>The fridge is empty</li>
-						)}
+					{fridge.food_items.length === 0 && (
+						<li>The fridge is empty</li>
+					)}
 
-						{fridge.food_items.map((item) => (
-							<li key={item.id}>
-								{item.name} — {item.share_status}
-							</li>
-						))}
+					{fridge.food_items.map((item) => (
+						<li key={item.id}>
+							<span>{item.name} — {item.share_status}</span>
+							<button onClick={() => deleteFoodItem(item.id)}>Delete</button>
+						</li>
+					))}
 				</ul>
 			</Container>
 		</>

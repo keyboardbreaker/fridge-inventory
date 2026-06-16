4e2115008b48e0a6d8389c4b704fe1893741abac
keyboardbreaker <yujin_1991@hotmail.com>
2026-06-16 21:32:52 +0100

    feat: header shows user's full name on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,48 @@
 import style from './header.module.css';
 import { signOut } from '../../../utils/auth';
 import type { User } from '@supabase/supabase-js';
+import { useEffect, useState } from 'react';
+import supabase from '../../../utils/supabase';
 
 type HeaderProps = {
-  user: User;
+    user: User;
 };
 
 const Header = ({ user } : HeaderProps) => {
+    const [fullName, setFullName] = useState<string>("");
+
+    useEffect(() => {
+        async function getFullName() {
+            const { data, error } = await supabase
+			.from("profiles")
+			.select(`
+				first_name,
+				last_name
+			`)
+			.eq("id", user.id)
+			.single();
+            
+            if (error) {
+                console.error(error);
+                return;
+            }
+            if (data) {
+                setFullName(`${data.first_name} ${data.last_name}`);
+            }
+        }
+        getFullName()
+    }, [user.id]);
+    
     return (
         <header className={style.header}> 
             <div className={style.headerContent}>
                 <h1>Fridge Inventory</h1>
-                <p>Manage your items without forgetting they exist </p>
 
                 <div className={style.userDetails}>
-                    <span>{user.email}</span>
+                    <span>Welcome: {fullName}</span>
                     <button onClick={signOut}>Logout</button>
                 </div>
+                <p>Manage your items without forgetting they exist </p>
             </div>
 
         </header>

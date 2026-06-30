285438c01f21b156f50908f3917f3f07f3c6288e
keyboardbreaker <yujin_1991@hotmail.com>
2026-06-30 23:57:59 +0100

    feat: home fridge card shows number of food items and when last updated with new item

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,26 +1,59 @@
 import { useState, useEffect } from 'react'
 import supabase from '../../../utils/supabase';
-import type { Fridge } from '../../types/fridge';
 import { Link } from 'react-router';
 import Loader from '../loader';
 import Container from '../container';
 import style from "./home.module.css";
+import { formatDistanceToNow } from "date-fns";
+
+type FridgeCard = {
+  id: string;
+  name: string;
+  itemCount: number;
+  lastUpdated: string | null;
+};
 
 const Home = () => {
-	const [fridges, setFridges] = useState<Fridge[]>([]);
+	const [fridges, setFridges] = useState<FridgeCard[]>([]);
 
   useEffect(() => {
     async function getFridges() {
       const { data, error } = await supabase
         .from("fridges")
-        .select("*");
+        .select(`
+          id,
+          name,
+          food_items (
+            id,
+            created_at
+          )
+        `);
         
       if (error) {
         console.error(error);
         return;
       }
       if (data) { //shorthand setFridges(data ?? []);
-        setFridges(data)
+        const fridgeCards: FridgeCard[] = data.map((fridge) => {
+          const itemCount = fridge.food_items.length;
+
+          const lastUpdated = fridge.food_items.length > 0 ?
+            fridge.food_items.reduce((latest, item) => {
+              if(!latest) return item.created_at;
+
+              return new Date(item.created_at) > new Date(latest) ?
+                item.created_at :
+                latest;
+            }, null as string | null) : null;
+
+            return {
+              id: fridge.id,
+              name: fridge.name,
+              itemCount,
+              lastUpdated
+            }
+        });
+        setFridges(fridgeCards);
       }
     }
 
@@ -44,8 +77,16 @@ const Home = () => {
                           <div className={style.grid}>
                             <div className={style.card}>
                               <h3>{fridge.name}</h3>
-                              <p className={style.items}>12 items</p>
-                              <p className={style.updated}>Last updated 2h ago</p>
+                              <p className={style.items}>{fridge.itemCount} item{fridge.itemCount !== 1 && "s"}</p>
+                              <p className={style.updated}>
+                                {
+                                  fridge.lastUpdated ?
+                                    `Last updated ${formatDistanceToNow(new Date(fridge.lastUpdated), {
+                                      addSuffix : true
+                                    })}` :
+                                    ""
+                                }
+                              </p>
                             </div>
                           </div>
                       </Link>

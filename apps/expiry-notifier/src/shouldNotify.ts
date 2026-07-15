777f6578704f14867795408346d79d6b1fa23524
keyboardbreaker <yujin_1991@hotmail.com>
2026-07-15 13:06:17 +0100

    comments: documenting should notify logic

--- apps/expiry-notifier/src/shouldNotify.ts
+++ apps/expiry-notifier/src/shouldNotify.ts
@@ -1,10 +1,14 @@
+/*
+    should I send a notification today 
+    if an item has expired on a given date.
+*/
 const shouldNotify = (expiry: string, notifyDaysBefore: number) => {
     const today = new Date();
     const expiryDate = new Date(expiry);
 
-    const days = Math.ceil(
-        (expiryDate.getTime() -  today.getTime())
-            /(1000 * 60 * 60 * 24)
+    const days = Math.ceil( //round up
+        (expiryDate.getTime() -  today.getTime()) //time till expiry in milliseconds
+            /(1000 * 60 * 60 * 24) //days from milliseconds
     );
 
     return days === notifyDaysBefore || notifyDaysBefore >= days;

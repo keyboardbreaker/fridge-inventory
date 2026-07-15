220d26304205d98a71c90b0cf87b6d5b07e1c5b4
keyboardbreaker <yujin_1991@hotmail.com>
2026-07-15 12:46:33 +0100

    getExpiringFood notify function test in handler

--- apps/expiry-notifier/src/handler.ts
+++ apps/expiry-notifier/src/handler.ts
@@ -1,9 +1,31 @@
-import getExpiringFood from "./notifications";
+import getExpiringFood, { ExpiringFood } from "./getExpiringFood";
+import shouldNotify from "./shouldNotify";
 
 const main = async () => {
-    const items = await getExpiringFood();
+    const items: ExpiringFood[] = await getExpiringFood();
 
-    console.log(items);
+    // console.log(items);
+
+    for (const item of items) {
+        if (!item.owner!.email_notifications)
+            continue;
+
+        if (!item.best_before_date)
+            continue;
+
+        if (
+            shouldNotify(
+                item.best_before_date,
+                item.owner!.notify_days_before
+            )
+        ) {
+            console.log(
+                `${item.name} expiring on 
+                ${item.best_before_date}, 
+                should send email`
+            );
+        }
+}
 };
 
 main();
